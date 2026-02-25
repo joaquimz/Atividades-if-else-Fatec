0cbce29740273e660440047dd0440b40a69ccf8d
Joaquim Teixeira <joaquimtalav@gmail.com>
2026-02-25 10:35:54 -0300

    Implement age classification logic
    Added age classification based on user input.

--- 2.py
+++ 2.py
@@ -1,6 +1,6 @@
-n1=int(input("Digite um numero:"))
-n2=int(input("digite um segundo numero:"))
-if n1 > n2:
-    print(f"{n1} maior que {n2}")
-else:n2 > n1
-print(f"{n2} e maior que {n1}")
+idade=int(input("Digite sua idade:"))
+if idade<18:
+    print("Menor de idade")
+elif idade>=65:
+     print("Idoso")
+else: print("Menor de Idade")
